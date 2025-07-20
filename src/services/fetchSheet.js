b265dba3adb068ef0f5d9f0bf150710dd4950f9d
dulsara30 <dulsaramanakal@gmail.com>
2025-07-21 00:05:32 +0530

    fetching data is changed

--- src/services/fetchSheet.js
+++ src/services/fetchSheet.js
@@ -1,11 +1,11 @@
 import Tabletop from 'tabletop';
 
-const SHEET_ID = '1GxNsJFj1YdjRicbLOLRIi94_qMSxEpNv0XXt9rVmQjE';
+const PUBLIC_SHEET_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTAzCyKYvnboR7hSMGfZhjUxUvTh97pvlSehop8bFPgxcGq0cn_hLhk30k1zEAIAS4JhFFFyAZVmK5d/pubhtml?gid=1823143616&single=true';
 
 export const fetchSheetData = () => {
     return new Promise((resolve, reject) => {
         Tabletop.init({
-            key: SHEET_ID,
+            key: PUBLIC_SHEET_URL,
             simpleSheet: true,
             callback: data => resolve(data),
             error: err => reject(err)
