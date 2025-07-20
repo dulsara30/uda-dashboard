a75a32ccffe84ce23831052a31d1a8a3599b1e33
dulsara30 <dulsaramanakal@gmail.com>
2025-07-20 23:55:18 +0530

    connect with spreadsheet

---
+++ src/services/fetchSheet.js
@@ -0,0 +1,14 @@
+import Tabletop from 'tabletop';
+
+const SHEET_ID = '2PACX-1vTAzCyKYvnboR7hSMGfZhjUxUvTh97pvlSehop8bFPgxcGq0cn_hLhk30k1zEAIAS4JhFFFyAZVmK5d';
+
+export const fetchSheetData = () => {
+    return new Promise((resolve, reject) => {
+        Tabletop.init({
+            key: SHEET_ID,
+            simpleSheet: true,
+            callback: data => resolve(data),
+            error: err => reject(err)
+        });
+    });
+};
